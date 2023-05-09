cf1a92c2b24be9b64e00d54ffefc327710f5a61b
Loic Correvon <pumbaa666@gmail.com>
2023-05-09 16:19:08 +0200

    Move infra and webapp into their own repositories. Will add them as submodule.

--- AWS/hello-world/aws-cli-setup/aws-cli-setup.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-# Source : https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-
-unzip awscliv2.zip
-sudo ./aws/install
--- AWS/hello-world/nodejs/.platform/hooks/predeploy/01_get_eb_env_name.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-BEANSTALK_ENV_NAME="$(/opt/elasticbeanstalk/bin/get-config container -k environment_name)"
-echo "Beanstalk environment name : $BEANSTALK_ENV_NAME"
