72a6ecdea7adeb8851c8f91d542c8610768568e5
Loic Correvon <pumbaa666@gmail.com>
2023-05-12 10:34:49 +0200

    cleaning

---
+++ AWS/aws-cli-setup/aws-cli-setup.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+# Source : https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+# to get the latest version :
+# curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
