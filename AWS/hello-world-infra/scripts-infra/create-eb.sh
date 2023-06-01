c7b55ab3b679d74c0c9eae33be78126fa627c4d2
Loic Correvon <pumbaa666@gmail.com>
2023-06-01 09:20:22 +0200

    create single instance

--- AWS/hello-world-infra/scripts-infra/create-eb.sh
+++ AWS/hello-world-infra/scripts-infra/create-eb.sh
@@ -7,9 +7,9 @@ REGION=eu-west-1 # Ireland
 INSTANCE_TYPE=t3.micro # Free tiers instance
 PLATFORM=node.js-18
 MIN_INSTANCES=1
-MAX_INSTANCES=2
+MAX_INSTANCES=1
 ELB_TYPE=network # Valid values: classic, application, network
-CNAME=hello-world
+CNAME=hello-world-single
 
 # Test if the folder .elasticbeanstalk exists
 if [ ! -d .elasticbeanstalk ]; then
@@ -26,20 +26,24 @@ echo "Load Balancer Type: $ELB_TYPE"
 echo "Min instances: $MIN_INSTANCES, Max instances: $MAX_INSTANCES"
 echo "SSH Keyname: $KEYNAME"
 
+# Multiple instances with load balancer
+# eb create dev-env --branch_default \
+#     --instance_type $INSTANCE_TYPE --platform $PLATFORM \
+#     --min-instances $MIN_INSTANCES --max-instances $MAX_INSTANCES \
+#     --region $REGION \
+#     --elb-type network \
+#     --cname $CNAME \
+#     --keyname $KEYNAME
+
+# Single instance
 eb create dev-env --branch_default \
-    --instance_type t3.micro --platform node.js-18 \
-    --min-instances 1 --max-instances 2 \
-    --region eu-west-1 \
-    --elb-type network \
-    --cname hello-world \
+    --instance_type $INSTANCE_TYPE --platform $PLATFORM \
+    --single \
+    --region $REGION \
+    --cname $CNAME \
     --keyname $KEYNAME
-    
     # --instance_profile pumbaa-admin \
     # --version test-0.0.1 \
-    # --envvars .ebextensions/environment.config
-    # --min-instances 1 --max-instances 2 \
-    # --single \
-    # --shared-lb-port 1337 \
     # --tags environment=test \
     
 if [ $? -ne 0 ]; then
